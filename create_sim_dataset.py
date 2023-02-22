e07d31e13d651f9eb9beb1455cdc1350ce1d0240
Rahul Mehta <rmehta1987@gmail.com>
2023-02-22 15:29:32 -0600

    changed plotting tool in loading checks and now we can create hdf5 dataset so we don't have to load all of it into memory

--- create_sim_dataset.py
+++ create_sim_dataset.py
@@ -3,6 +3,8 @@ from itertools import islice
 from sortedcontainers import SortedDict
 import re
 import os
+import hdfdict
+import h5py
 
 def closest(sorted_dict, key):
     "Return closest key in `sorted_dict` to given `key`."
@@ -31,13 +33,38 @@ def get_data_and_put(data_path: str, file_name: str,):
     print('Finished creating sorted-dictionary')
     np.save(file_name, sel_sfs_dict, allow_pickle=True)
 
+def convert_dict_to_hdf5(data_path: str, file_name: str):
+    thedict = np.load(data_path, allow_pickle=True).item()
+    with h5py.File(file_name, "w") as the_file:
+        hdfdict.dump(thedict, the_file)
+
+def create_hdf5_dataset(data_path: str, file_name: str):
+
+        # First filter out files to match specific file; ie sfs...{}.txt
+    lsdirs = os.listdir(data_path)
+    regex_remove_last = re.compile(r'((?!last).)*$')
+    sel_list_files = list(filter(regex_remove_last.match,lsdirs))
+    
+    regex_capture_selection_files = re.compile(r'^.*[{|}].*$')
+    sel_list_files = list(filter(regex_capture_selection_files.match, sel_list_files))
+    regex_capture_selection_coef = re.compile(r'(?<={)(.*)(?=})')
+    
+    with h5py.File(file_name, "w") as the_file:
+        for a_file in sel_list_files:
+            full_file_path = f'{data_path}{a_file}'
+            sel_coef = regex_capture_selection_coef.search(a_file)[1] # gets the actual selection coefficient
+            the_sfs = np.loadtxt(full_file_path, dtype=float)[1:] # first line is number of mutations
+            the_file.create_dataset(sel_coef, data=the_sfs)
+    
+    print('Finished creating hdf5 dataset')
 
 def main():
 
     missense_data_path = '/home/rahul/PopGen/SimulationSFS/ParallelPopGen_Data/sfs_sims_missense_avg_rate/'
     lof_data_path = '/home/rahul/PopGen/SimulationSFS/ParallelPopGen_Data/sfs_sims_lof_avg_rate/'
-    file_name = 'sfs_lof_data'
-    get_data_and_put(lof_data_path, file_name)
+    lof_data = 'sfs_lof_sim_data.npy'
+    file_name = 'sfs_lof_hdf5_test_data.h5'
+    create_hdf5_dataset(lof_data_path, file_name)
 
 
 if __name__ == "__main__":
--- loading_checks.py
+++ loading_checks.py
@@ -28,18 +28,22 @@ import sparselinear as sl
 from sortedcontainers import SortedDict
 from scipy.spatial import KDTree
 
+logging.getLogger('matplotlib').setLevel(logging.ERROR) # See: https://github.com/matplotlib/matplotlib/issues/14523
 
 class SummaryNet(nn.Module):
     def __init__(self, sample_size):
         super().__init__()
         self.sample_size = sample_size
-        self.linear1 = sl.SparseLinear(self.sample_size, int(self.sample_size / 400))
-        self.linear2 = sl.SparseLinear(int(self.sample_size / 400), int(self.sample_size / 400))
-        self.linear3 = sl.SparseLinear(int(self.sample_size / 400), int(self.sample_size / 600))
-        self.linear4 = sl.SparseLinear(int(self.sample_size / 600), int(self.sample_size / 800))
-        self.linear5 = sl.SparseLinear(int(self.sample_size / 800), int(self.sample_size / 1200))
+        self.linear1 = sl.SparseLinear(self.sample_size, int(self.sample_size / 200)) # 585
+        self.bn1 = nn.LazyBatchNorm1d(int(self.sample_size / 200))
+        self.linear2 = sl.SparseLinear(int(self.sample_size / 200), int(self.sample_size / 200)) # 585
+        self.linear3 = sl.SparseLinear(int(self.sample_size / 200), int(self.sample_size / 400)) # 283
+        self.bn2 = nn.LazyBatchNorm1d(int(self.sample_size / 400))
+        self.linear4 = sl.SparseLinear(int(self.sample_size / 400), int(self.sample_size / 600)) # 195
+        self.bn3 = nn.LazyBatchNorm1d(int(self.sample_size / 600))
+        self.linear5 = sl.SparseLinear(int(self.sample_size / 600), int(self.sample_size / 600)) # 195
 
-        self.model = nn.Sequential(self.linear1, nn.SiLU(), self.linear2, nn.SiLU(), self.linear3, nn.SiLU(), self.linear4, nn.SiLU(), self.linear5)
+        self.model = nn.Sequential(self.linear1, self.bn1, nn.SiLU(), self.linear2, nn.SiLU(), self.linear3, self.bn2, nn.SiLU(), self.linear4, self.bn3, nn.SiLU(), self.linear5)
 
     def forward(self, x):
         
@@ -47,24 +51,62 @@ class SummaryNet(nn.Module):
         return x
     
 
-obs = torch.load('/home/rahul/PopGen/SimulationSFS/Experiments/saved_posteriors_nfe_infer_missense_selection_1_2023-02-15_13-42/posterior_last_round.pkl')
-obs1 = torch.load('/home/rahul/PopGen/SimulationSFS/Experiments/saved_posteriors_nfe_infer_missense_selection_1_2023-02-15_13-42/posterior_observed_round_5.pkl')
-obs0 = torch.load('/home/rahul/PopGen/SimulationSFS/Experiments/saved_posteriors_nfe_infer_missense_selection_1_2023-02-15_13-42/posterior_observed_round_0.pkl')
+obs = torch.load('/home/rahul/PopGen/SimulationSFS/Experiments/saved_posteriors_nfe_infer_missense_selection_1_2023-02-22_11-38/posterior_observed_round_10.pkl')
+obs1 = torch.load('/home/rahul/PopGen/SimulationSFS/Experiments/saved_posteriors_nfe_infer_missense_selection_1_2023-02-22_11-38/posterior_observed_round_5.pkl')
+obs0 = torch.load('/home/rahul/PopGen/SimulationSFS/Experiments/saved_posteriors_nfe_infer_missense_selection_1_2023-02-22_11-38/posterior_observed_round_0.pkl')
 
 sampleobs = obs.sample((100000,))
 sampleobs1 = obs1.sample((100000,))
 sampleobs0 = obs0.sample((100000,))
+bins = [0, 10e-5, 10e-4, 10e-3, 10e-2, 10e-1]
 
-temp = torch.cat((sampleobs, sampleobs1, sampleobs0),dim=1)
-temp = torch.log10(torch.abs(temp.squeeze()))
+temp = -1*torch.cat((sampleobs, sampleobs1, sampleobs0),dim=1)
+temp2 = torch.log10(torch.abs(temp.squeeze()))
 print(temp.shape)
-df = pd.DataFrame(temp.cpu().numpy(), columns=['Learned Prior', 'Training Round 1', 'Initial Proposal'])
-sns.kdeplot(df, label=['Learned Prior', 'Training Round 1', 'Initial Proposal'])
+df = pd.DataFrame(temp.cpu().numpy(), columns=['Training Round 10', 'Training Round 1', 'Initial Proposal'])
+df2 = pd.DataFrame(temp2.cpu().numpy(), columns=['Training Round 10', 'Training Round 1', 'Initial Proposal'])
+sns.kdeplot(df, label=['Training Round 10', 'Training Round 1', 'Initial Proposal'])
 #plt.legend(['Learned Prior', 'Proposal'])
 #plt.legend()
 plt.xlabel('Unscaled Selection (s)')
 plt.ylabel('Density')
 plt.title('Density Selection Coefficients Sampled from Inferred Distributions of round')
 plt.tight_layout()
-plt.savefig('para_log.png')
+plt.savefig('para_lof.png')
 plt.close()
+
+#plt.hist(np.log10(temp[:,0].cpu().numpy()),bins=[-5, -4, -3, -2, -1, 0], edgecolor='black', linewidth=1.2, histtype='bar')
+#plt.hist(np.log10(temp[:,1].cpu().numpy()),bins=[-5, -4, -3, -2, -1, 0], edgecolor='black', linewidth=1.2, histtype='bar')#plt.legend(['Learned Prior', 'Proposal'])
+#plt.hist(np.log10(temp[:,2].cpu().numpy()),bins=[-5, -4, -3, -2, -1, 0], edgecolor='black', linewidth=1.2, histtype='bar')#plt.legend(['Learned Prior', 'Proposal'])
+
+plt.hist(temp2.cpu().numpy(), bins=[-5, -4, -3, -2, -1, 0], linewidth=1.2, histtype='step', stacked=True, fill=False)
+
+plt.xlabel('Unscaled Selection (s)')
+plt.ylabel('Density')
+plt.title('Density Selection Coefficients Sampled from Inferred Distributions of round')
+plt.tight_layout()
+plt.savefig('para_lof_hist2.png')
+plt.close()
+
+sns.kdeplot(df2, label=['Training Round 10', 'Training Round 1', 'Initial Proposal'])
+#plt.legend(['Learned Prior', 'Proposal'])
+#plt.legend()
+plt.xlabel('Unscaled Selection (s)')
+plt.ylabel('Density')
+plt.title('Density Selection Coefficients Sampled from Inferred Distributions of round')
+plt.tight_layout()
+plt.savefig('para_lof_log.png')
+plt.close()
+
+the_device='cuda:0'
+
+box_uniform_prior = utils.BoxUniform(low=-0.015 * torch.ones(1, device=the_device), high=-1e-8*torch.ones(1,device=the_device),device=the_device)
+accept_reject_fn = get_density_thresholder(obs, quantile=1e-6)
+proposal = RestrictedPrior(box_uniform_prior, accept_reject_fn, obs, sample_with="sir", device=the_device)
+
+dfe = proposal.sample((100000,))
+temp3 = -1*torch.cat((sampleobs, dfe),dim=1)
+df3 = pd.DataFrame(temp3.cpu().numpy(), columns=['Restricted prior', 'Training Round 10'])
+sns.kdeplot(df3, label=['Restricted Round 10', 'Training Round 10'])
+
+plt.savefig('para_dfe.png')
